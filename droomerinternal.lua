fec10176827d29dd280cc6a897e536fded2b188b
DroomerTeam <scpnoob999@gmail.com>
2025-05-21 20:01:08 +0200

    Update droomerinternal.lua

--- droomerinternal.lua
+++ droomerinternal.lua
@@ -1,9 +1,9 @@
 local originalRequest = getgenv().request or function(options) return nil end
 getgenv().request = function(options)
     if options.Headers then
-        options.Headers['User-Agent'] = 'Droomer'
+        options.Headers['User-Agent'] = 'Droomer/2.0.0'
     else
-        options.Headers = {['User-Agent'] = 'Droomer'}
+        options.Headers = {['User-Agent'] = 'Droomer/2.0.0'}
     end
     return originalRequest(options)
 end
