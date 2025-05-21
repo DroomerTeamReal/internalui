c0d4eaf5d900c3e331cdd10707c8557d6a25937d
DroomerTeam <scpnoob999@gmail.com>
2025-05-21 20:00:42 +0200

    Update droomerinternal.lua

--- droomerinternal.lua
+++ droomerinternal.lua
@@ -1,1 +1,9 @@
-print("Internal UI Is Currently Down")
+local originalRequest = getgenv().request or function(options) return nil end
+getgenv().request = function(options)
+    if options.Headers then
+        options.Headers['User-Agent'] = 'Droomer'
+    else
+        options.Headers = {['User-Agent'] = 'Droomer'}
+    end
+    return originalRequest(options)
+end
